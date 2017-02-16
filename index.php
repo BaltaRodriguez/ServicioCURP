a586765ccb2dfa94e8a6f4c4475a8164b96ad7e8
BaltaRodriguez <rtellez@mx1.ibm.com>
2017-02-16 14:50:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+<?
+
+/**
+ * @author Victor Arturo Hernandez Avila
+ * @mail arturo[dot]webrek[at]gmail[dot]com
+ * @copyright 2009
+ */
+ 
+set_time_limit(0);
+function getCurp($primerApellido, $segundoApellido, $nombre, $diaNacimiento, $mesNaciemiento, $anioNacimiento, $sexo, $entidadNacimiento){
+$primerApellido = urlencode($primerApellido);
+$segundoApellido = urlencode($segundoApellido);
+$nombre = urlencode($nombre);
+$aContext = array(
+    'http' => array(
+        'header'=>"Accept-language: es-es,es;q=0.8,en-us;q=0.5,en;q=0.3\r\n" .
+              "Proxy-Connection: keep-alive\r\n" .
+              "Host: consultas.curp.gob.mx\r\n" .
+              "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; es-ES; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)\r\n" .
+              "Keep-Alive: 300\r\n" .
+              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+              //, 'proxy' => 'tcp://proxy:puerto', //Si utilizas algun proxy para salir a internet descomenta esta linea y por la direccion de tu proxy y el puerto
+              //'request_fulluri' => True //Tambien esta si utilizas algun proxy
+
+        ),
+    );
+$cxContext = stream_context_create($aContext);
+$url = "http://consultas.curp.gob.mx/CurpSP/curp1.do?strPrimerApellido=$primerApellido&strSegundoAplido=$segundoApellido&strNombre=$nombre&strdia=$diaNacimiento&strmes=$mesNaciemiento&stranio=$anioNacimiento&sSexoA=$sexo&sEntidadA=$entidadNacimiento&rdbBD=myoracle&strTipo=A&entfija=DF&depfija=04";
+    $file = file_get_contents($url, false, $cxContext);
+    preg_match_all("/var strCurp=\"(.*)\"/", $file, $curp);
+$curp = $curp[1][0];
+    if($curp){
+        return $curp;
+    }else{
+        $curp = "Curp no encontrado.";
+        return $curp;
+    }
+}
+?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-15073642-1");
+pageTracker._setDomainName("none");
+pageTracker._setAllowLinker(true);
+pageTracker._trackPageview();
+} catch(err) {}</script>
